71392faeb14028aa6bc86f3ddd0aa5fe5ab84e6c
Benoit <benoit.misplon.etu@univ-lille.fr>
2023-04-11 14:49:35 +0200

    modif projets

--- js/main.js
+++ js/main.js
@@ -82,3 +82,27 @@
     
 })(jQuery);
 
+document.querySelector('.aAgar').addEventListener('click', event => {
+    event.preventDefault();
+    document.querySelector('.pAgar').innerHTML = `Dungeon and Dragons est un jeu vidéo multijoueur free-to-play développé par Benoît Misplon et Antonin Duvauchel. Le joueur contrôle un cercle coloré représentant une cellule, dont le but est de grossir le plus possible en absorbant des cellules plus petites sans être à son tour absorbée par des cellules plus grosses.`;
+    document.querySelector('.aAgar').style.display = 'none';
+})
+document.querySelector('.aGlyphe').addEventListener('click', event => {
+    event.preventDefault();
+    document.querySelector('.pGlyphe').innerHTML = `
+    Il s'agit d'un site web pour une entreprise fictive. Durant ce projet nous avons du réliser le site d'une entreprise que nous avons creé, celle-ci vendait donc un moteur de jeu.
+    `;
+    document.querySelector('.aGlyphe').style.display = 'none';
+})
+
+document.querySelector('.aCloudy').addEventListener('click', event => {
+    event.preventDefault();
+    document.querySelector('.pCloudy').innerHTML = `
+    Le but de ce projet etait de développer un outil de chargement, classification, affichage, et consultation d'un ensemble de données <br>
+    La problématique professionnelle etait de mettre en place l'organisation du projet et de créer, en équipe, une application en suivant une démarche de développement itérative et incrémentale. En partant d'un besoin décrit de manière imprécise ou incomplète par un client, l'objectif est de clarifier, compléter, collecter et formaliser le besoin, puis de développer une application communicante intégrant la manipulation des données et respectant les paradigmes de qualité (ergonomie des IHM, qualité logicielle…).
+    `;
+    document.querySelector('.aCloudy').style.display = 'none';
+})
+
+
+
