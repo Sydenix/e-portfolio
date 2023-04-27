be5e42abc13aede438de1bfaef6aa5c3c0ca3297
Benoit <benoit.misplon.etu@univ-lille.fr>
2023-04-27 17:30:47 +0200

    add popup on projects

--- js/main.js
+++ js/main.js
@@ -82,23 +82,55 @@
     
 })(jQuery);
 
+const first = document.getElementById('1er');
+const seconde = document.getElementById('2eme');
+const third = document.getElementById('3eme');
+const fourth = document.getElementById('4eme');
+const fivth = document.getElementById('5eme');
+const sixth = document.getElementById('6eme');
+const seventh = document.getElementById('7eme');
+const eighth = document.getElementById('8eme');
+    popup(first);
+    popup(seconde);
+    popup(third);
+    popup(fourth);
+    popup(fivth);
+    popup(sixth);
+    popup(seventh);
+    popup(eighth);
+    
+    
+    function popup(popup){
+        popup.innerHTML += `
+        <btn id='exit' class="btn hidden exit btn-primary btn-lg-square rounded-0"><i class='bi bi-x-circle'></i></btn>
+        `
+        const exit = popup.querySelector('.btn');
+        popup.addEventListener('click', event => {
+        event.stopPropagation();
+        popup.classList.add('project-popup');
+        document.querySelector('body').style.overflow = 'hidden';
+        exit.classList.remove('hidden');
+    });
+    exit.addEventListener('click', event => {
+        event.stopPropagation();
+        event.preventDefault();
+        popup.classList.remove('project-popup');
+        document.querySelector('body').style.overflow = 'auto';
+        exit.classList.add('hidden');
+    })
+}
 document.querySelector('.aAgar').addEventListener('click', event => {
     event.preventDefault();
+    event.stopPropagation();
     document.querySelector('.pAgar').innerHTML = `Dungeon and Dragons est un jeu vidéo multijoueur free-to-play développé par Benoît Misplon et Antonin Duvauchel. Le joueur contrôle un cercle coloré représentant une cellule, dont le but est de grossir le plus possible en absorbant des cellules plus petites sans être à son tour absorbée par des cellules plus grosses.
     Le jeu se présente sous la forme d'un plateau, vu de dessus, et sur lequel se rejoignent toutes les personnes connectées : chacun.e peut ainsi voir en temps réel les mouvements des autres participant.e.s.
     `;
     document.querySelector('.aAgar').style.display = 'none';
 })
-document.querySelector('.aGlyphe').addEventListener('click', event => {
-    event.preventDefault();
-    document.querySelector('.pGlyphe').innerHTML = `
-    Il s'agit d'un site web pour une entreprise fictive. Durant ce projet nous avons du réliser le site d'une entreprise que nous avons creé, celle-ci vendait donc un moteur de jeu.
-    `;
-    document.querySelector('.aGlyphe').style.display = 'none';
-})
 
 document.querySelector('.aCloudy').addEventListener('click', event => {
     event.preventDefault();
+    event.stopPropagation();
     document.querySelector('.pCloudy').innerHTML = `
     Le but de ce projet etait de développer un outil de chargement, classification, affichage, et consultation d'un ensemble de données <br>
     La problématique professionnelle etait de mettre en place l'organisation du projet et de créer, en équipe, une application en suivant une démarche de développement itérative et incrémentale. En partant d'un besoin décrit de manière imprécise ou incomplète par un client, l'objectif est de clarifier, compléter, collecter et formaliser le besoin, puis de développer une application communicante intégrant la manipulation des données et respectant les paradigmes de qualité (ergonomie des IHM, qualité logicielle…).
@@ -108,15 +140,17 @@ document.querySelector('.aCloudy').addEventListener('click', event => {
 
 document.querySelector('.aUniv').addEventListener('click', event => {
     event.preventDefault();
+    event.stopPropagation();
     document.querySelector('.pUniv').innerHTML = `
-Il s'agit d'un site web permettant la mise en place d'un système de co-voiturage et de location entre les étudiants et le personnel de l'université de Lille. Ce projet a été réalisé en binome , il s'agit simplement d'un site vitrine.
-   
+    Il s'agit d'un site web permettant la mise en place d'un système de co-voiturage et de location entre les étudiants et le personnel de l'université de Lille. Ce projet a été réalisé en binome , il s'agit simplement d'un site vitrine.
+    
     `;
     document.querySelector('.aUniv').style.display = 'none';
 })
 
 document.querySelector('.aRest').addEventListener('click', event => {
     event.preventDefault();
+    event.stopPropagation();
     document.querySelector('.pRest').innerHTML = `
     Réalisation de l'API Rest d'une pizzeria en Java. Requêtes GET/POST/PATCH/DELETE mises en place avec un lien à une base de données PostgreSQL.
     Gestion des ingrédients, pizzas et des commandes. Sécurisation de l'API avec un système de gestion d'authentification avec une basic authent.
@@ -124,5 +158,11 @@ document.querySelector('.aRest').addEventListener('click', event => {
     document.querySelector('.aRest').style.display = 'none';
 })
 
-
-
+document.querySelector('.aGlyphe').addEventListener('click', event => {
+    event.preventDefault();
+    event.stopPropagation();
+    document.querySelector('.pGlyphe').innerHTML = `
+    Il s'agit d'un site web pour une entreprise fictive. Durant ce projet nous avons du réliser le site d'une entreprise que nous avons creé, celle-ci vendait donc un moteur de jeu.
+    `;
+    document.querySelector('.aGlyphe').style.display = 'none';
+})
